7fcae5cab1cb64c78b01719250b2fbf2d5e8613f
OverTheStars-UG <69696958+OverTheStars-UG@users.noreply.github.com>
2022-02-15 23:53:14 +0100

    Add files via upload

---
+++ style/main.js
@@ -0,0 +1,109 @@
+window.onscroll = function() {sticnav()};
+
+var navbar = document.getElementById("sticky-nav");
+var sticky = navbar.offsetTop;
+
+function sticnav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function respnav() {
+  if (navbar.className === "topnav" || navbar.className === "topnav sticky" ) {
+  navbar.classList.add("responsive");
+  } 
+
+  else {
+    navbar.classList.remove("responsive")
+  }
+
+}
+
+
+//SLIDE
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+/* Slide Automatica
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Cambia ogni 5 secondi
+}*/
+
+
+function openCountry(evt, cityName) {
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+//Bottone torna su
+  var bottonesu = document.getElementById("tornasu");
+
+  window.onscroll = function() {togglaBottone()};
+
+  function togglaBottone() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          bottonesu.style.display = "block";
+        } else {
+                bottonesu.style.display = "none";
+        }
+        }
+
+// When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
